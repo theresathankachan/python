7c3a4128376f09c9637c30c1e8a638614be39b8a
theresathankachan <88645655+theresathankachan@users.noreply.github.com>
2022-02-08 16:09:28 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,12 @@
+p=int(input("enter percentage"))
+if p>90:
+      print("Grade=O")
+elif p>=80 and p<90:
+      print("Grade=A")
+elif p>=70 and p<80:
+      print("Grade=B")
+elif p>=50 and p<70:
+      print("Grade=C")
+else:
+      print("Grade=f")
+
